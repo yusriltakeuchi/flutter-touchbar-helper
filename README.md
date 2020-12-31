943d0ae91722b2184fbf0b5f5d6f4b6ba184d383
Yusril Rapsanjani <yusriltakeuchi@gmail.com>
2020-12-31 10:41:53 +0700

    First push

---
+++ README.md
@@ -0,0 +1,24 @@
+# flutter-touchbar-helper
+This is the touchbar helper for Flutter, so you can save your time to doing some action like flutter clean and build. And this is also include with git push and commit.
+
+![Screenshot Main](https://leeyurani.com/Screenshot-1.png)
+![Screenshot Build](https://leeyurani.com/Screenshot-2.png)
+![Screenshot Git](https://leeyurani.com/Screenshot-3.png)
+
+## Features
+- Flutter Clean
+- Flutter Build Single APK
+- Flutter Build Appbundle
+- Flutter Build Split per ABI
+- Git Commit
+- Git Push
+
+## Release Notes
+
+This is the release notes
+
+### 1.0.0
+
+Initial release of flutter-helper
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,76 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+export function activate(context: vscode.ExtensionContext) {
+	/// Activate flutter build
+	const buildFlutter = vscode.commands.registerCommand('flutter-touchbar-helper.flutterbuild', () => {
+		vscode.commands.executeCommand('setContext', 'enableBuild', true);
+	});
+
+	/// Closing build options
+	const closeBuild = vscode.commands.registerCommand('flutter-touchbar-helper.closebuild', () => {
+		vscode.commands.executeCommand('setContext', 'enableBuild', false);
+	});
+	/// Closing git
+	const closeGit = vscode.commands.registerCommand('flutter-touchbar-helper.closegit', () => {
+		vscode.commands.executeCommand('setContext', 'enableGit', false);
+	});
+	
+	const git = vscode.commands.registerCommand('flutter-touchbar-helper.git', () => {
+		vscode.commands.executeCommand('setContext', 'enableGit', true);
+	});
+
+	// Git commit 
+	const commit = vscode.commands.registerCommand('flutter-touchbar-helper.gitcommit', () => {
+		vscode.commands.executeCommand('git.commit');
+		vscode.commands.executeCommand('setContext', 'enableGit', false);
+	});
+
+	/// Git push
+	const push = vscode.commands.registerCommand('flutter-touchbar-helper.gitpush', () => {
+		vscode.commands.executeCommand('git.push');
+		vscode.commands.executeCommand('setContext', 'enableGit', false);
+	});
+
+	/// Activate build single apk
+	const buildSingleApk = vscode.commands.registerCommand('flutter-touchbar-helper.flutterbuild.singleapk', () => {
+		vscode.commands.executeCommand('setContext', 'enableBuild', false);
+		const terminal = vscode.window.createTerminal('Flutter Build APK');
+		terminal.show();
+		terminal.sendText("flutter clean");
+		terminal.sendText("flutter build apk --release");
+	});
+
+	/// Activate build single appbundle
+	const buildSingleAppbundle = vscode.commands.registerCommand('flutter-touchbar-helper.flutterbuild.singleappbundle', () => {
+		vscode.commands.executeCommand('setContext', 'enableBuild', false);
+		const terminal = vscode.window.createTerminal('Flutter Build Appbundle');
+		terminal.show();
+		terminal.sendText("flutter clean");
+		terminal.sendText("flutter build appbundle --release");
+	});
+
+	/// Activate build split per abi
+	const buildSplitABI = vscode.commands.registerCommand('flutter-touchbar-helper.flutterbuild.splitabi', () => {
+		vscode.commands.executeCommand('setContext', 'enableBuild', false);
+		const terminal = vscode.window.createTerminal('Flutter Build ABI');
+		terminal.show();
+		terminal.sendText("flutter clean");
+		terminal.sendText("flutter build apk --split-per-abi --release");
+	});
+
+	/// Activat eflutter clean
+	const cleanFlutter = vscode.commands.registerCommand('flutter-touchbar-helper.flutterclean', () => {
+		const terminal = vscode.window.createTerminal('Flutter Clean Terminal');
+		terminal.show();
+		terminal.sendText("flutter clean");
+	});	
+
+	context.subscriptions.push(cleanFlutter, buildFlutter, buildSingleApk, buildSingleAppbundle, buildSplitABI, closeBuild, git, closeGit, commit, push);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
